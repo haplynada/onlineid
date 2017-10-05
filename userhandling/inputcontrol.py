c6c3020db90d0d811510d225ea9de1fecbbe3152
Tor Larssen Sekse <tlskse@gmail.com>
2017-10-05 14:35:03 +0200

    update to inputcontrol

--- userhandling/inputcontrol.py
+++ userhandling/inputcontrol.py
@@ -3,20 +3,34 @@ Created on 5. okt. 2017
 
 @author: Tor Larssen Sekse
 '''
-validchars = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-_ @')
+validchars_email = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-_ @')
+validchars_name = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+
 def check_password(password):
     password_length = len(password)
     if password_length >= 8  and (any(char.isdigit() for char in password)) and password_length <=15: 
         return True
-    else :
+    else:
         return "invalid password"
 
 def check_email(email):
-    if set(email).issubset(validchars):
+    if set(email).issubset(validchars_email):
         if "@" and "." in email: 
             return True
     else:
         return "invalid email"
+    
+def check_firstname(name):
+    if set(name).issubset(validchars_name):
+        return True
+    else:
+        return "invalid first name"
+    
+def check_lastname(name):
+    if set(name).issubset(validchars_name):
+        return True
+    else:
+        return "invalid last name"
     
     
 def check_phonenumber(phone):
@@ -24,6 +38,19 @@ def check_phonenumber(phone):
     phone_length = len(phone)
     if phone_length == 8 and (phone.isdigit()):  
         return True
-    else:
         return "invalid phone number"
-
+    
+def check_adress(adress):
+    if set(adress).issubset(validchars_name):
+        return True
+    else:
+        return "invalid adress"
+    
+def check_postcode(postcode):
+    postcode_length = len(postcode)
+    if postcode_length == 4 and postcode.isdigit():
+        return True
+    else:
+        return "invalid postcode"
+    
+    
