8b6933b36bed553946561e6bfc5962f1d2c64031
unknown <alexmackzie@hotmail.com>
2017-10-12 13:43:28 +0200

    added get hash and salt functions from user_id

--- userhandling/getuserdata.py
+++ userhandling/getuserdata.py
@@ -1,19 +1,38 @@
 '''
 Created on 5. okt. 2017
 
-@author: Tor Larssen Sekse
-'''
+@author: Tor Larssen Sekse/Alexander Mackenzie-Low
+''' 
+import pymysql
 
 def get_hashedpassword(user):
-    hashedpassword = 
-    '''
-    this part needs to call on the database using the userid and get the hashedpassword
-    '''
-    return hashedpassword
+    
+    db = pymysql.connect(host="127.0.0.1",  # your host 
+        user="root",       # username
+        passwd="root",     # password
+        db="OnlineID")   # name of the database
+ 
+# Create a Cursor object to execute queries.
+    cur = db.cursor()
+    query = "SELECT hashed_Passwords from information WHERE user_id =%s;"
+    cur.execute(query, (user))
+# filter and return result  
+    result = str(cur.fetchone()[0])
+    return(result)
+    print(result)
 
 def get_salt(user):
-    salt =
-        '''
-    this part needs to call on the database using the userid and get the salt
-    '''
-    return salt
+    
+    db = pymysql.connect(host="127.0.0.1",  # your host 
+        user="root",       # username
+        passwd="root",     # password
+        db="OnlineID")   # name of the database
+ 
+# Create a Cursor object to execute queries.
+    cur = db.cursor()
+    query = "SELECT salt from information WHERE user_id =%s;"
+    cur.execute(query, (user))
+# filter and return result  
+    result = str(cur.fetchone()[0])
+    return(result)
+    print(result)
