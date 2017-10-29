aaefd9ab9e8c5814ab1ca76507e6057168343c8a
Tor Larssen Sekse <tlskse@gmail.com>
2017-10-29 10:15:14 +0100

    no message

--- main/apiconnection.py
+++ main/apiconnection.py
@@ -8,7 +8,7 @@ import ssl
 from _socket import SOL_SOCKET, SO_REUSEADDR
 from userhandling.authenticateuser import authenticate_user
 import userhandling.getuserdata
-from userhandling.getuserdata import get_email
+from userhandling.getuserdata import get_email, get_firstname, get_lastname
 from Databasehandling.queries import getuser
 
 def handle_data(connstream, data):
@@ -29,18 +29,33 @@ def handle_data(connstream, data):
         print(authenticate_user(datalist[0], datalist[1]))
     elif datalist[0] == "edituser":
         del datalist[0]
+        
     elif datalist[0] == "getdata":
         del datalist[0]
+        
         if datalist[0] == "getfirstname":
             del datalist[0]
-            getuser(datalist[0])
-            
+            user= getuser(datalist[0])
+            return_data = "getdata|firstname|" + str(get_firstname(user))
+            return_data.encode(encoding='utf_8')
+            connstream.send(return_data)
+            return True
+        
+        elif datalist[0] == "getlastname": 
+            del datalist[0]
+            user = getuser(datalist[0])
+            user = get_lastname(user)
+            return_data = "getdata|lastname|" + str(get_lastname(user))
+            return_data.encode(encoding='utf_8')
+            connstream.send(return_data)
+            return True
+        elif datalist[0] == ""
     else:
         print("fuck")
         print(datalist)
     return False
 
-def receive_data(connstream):
+def receive_data(connstream):   
     data=connstream.read()
     while data:
         if not handle_data(connstream, data):
