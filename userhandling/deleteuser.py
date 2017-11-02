fcd1587340b8b8b0c86acbc45ff86d00804f318f
unknown <alexmackzie@hotmail.com>
2017-11-02 10:53:23 +0100

    delte user

---
+++ userhandling/deleteuser.py
@@ -0,0 +1,23 @@
+'''
+Created on Nov 2, 2017
+ 
+@author: Bjarke Larsen
+'''
+import pymysql
+ 
+def deleteuser(user_id):
+   
+    db = pymysql.connect(host="88.88.170.2",  # your host
+         user="server",       # username
+         passwd="sudoonlineid",     # password
+         db="OnlineID" # name of the database, commented out since I am creating the DB in the string below
+         )  
+ 
+# Create a Cursor object to execute queries.
+    cur = db.cursor()
+   
+    query = "delete FROM information where user_id =%s;"
+    cur.execute(query, (user_id))
+# filter and return result  
+    result = str(cur.fetchone()[0])
+    return(result)
