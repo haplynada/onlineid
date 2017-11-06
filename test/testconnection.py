6045de9648e62bdbe3e162b74382c430a19646a1
Tor Larssen Sekse <tlskse@gmail.com>
2017-11-06 13:06:11 +0100

    added timing to testconnection

--- test/testconnection.py
+++ test/testconnection.py
@@ -8,22 +8,29 @@ data used for testing purposes
 =b"newuser|sau@sau.no|koktsau42|Sau|Sausen|12398456|2520|Norway|0047|Faareveien|42|2017-01-01|female"
 '''
 import socket, ssl
+import timeit
 
-sender = socket.socket()
-host = socket.gethostname()
-sender_ssl = ssl.wrap_socket(sender)
-
-sender_ssl.connect(("88.88.170.2", 22025))
-
-print(sender_ssl.getpeername())
-send_data =b"newuser|sau@sau.no|koktsau42|Sau|Sausen|12398456|2520|Norway|0047|Faareveien|42|2017-01-01|female"
+#testdata
+send_data =b"newuser|saua@sau.no|koktsau42|Sau|Sausen|12398456|2520|Norway|0047|Faareveien|42|2017-01-01|female"
 send_data2 =b"getdata|sau@sau.no|koktsau42|getgender"
 send_login =b"login|sau@sau.no|koktsau42"
 send_deleteuser =b"deleteuser|sau@sau.no|koktsau42"
 send_getall = b"getalldata|sau@sau.no|koktsau42"
+#testdata end
 
-sender_ssl.send(send_data)
-print(sender_ssl.recv().decode())
+def connect():
+    sender = socket.socket()
+    host = socket.gethostname()
+    sender_ssl = ssl.wrap_socket(sender)
 
-sender_ssl.close()
+    sender_ssl.connect(("88.88.170.2", 22025))
 
+    print(sender_ssl.getpeername())
+
+
+    sender_ssl.send(send_login)
+    print(sender_ssl.recv().decode())
+
+    sender_ssl.close()
+
+print(timeit.timeit(connect, number = 100))
