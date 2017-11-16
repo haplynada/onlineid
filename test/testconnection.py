1c81579c78d6d241546c2e866c6aad0a3e46415d
Tor Larssen Sekse <tlskse@gmail.com>
2017-11-16 14:32:57 +0100

    error corrections

--- test/testconnection.py
+++ test/testconnection.py
@@ -11,7 +11,7 @@ import socket, ssl
 import timeit
 
 #testdata
-send_data =b"newuser|sausau@sau.no|koktsau42|Sau|Sausen|12398456|2520|Norway|0047|Faareveien|42|2004-01-01|female"
+send_data =b"newuser|sau@sau.no|koktsau42|Sau|Sausen|12398456|2520|Norway|0047|Faareveien|42|2004-01-01|female"
 send_data2 =b"getdata|sau@sau.no|koktsau42|getgender"
 send_login =b"login|sau@sau.no|koktsau42"
 send_deleteuser =b"deleteuser|sau@sau.no|koktsau42"
@@ -28,7 +28,7 @@ def connect():
     print(sender_ssl.getpeername())
 
 
-    sender_ssl.send(send_data)
+    sender_ssl.send(send_login)
     print(sender_ssl.recv().decode())
 
     sender_ssl.close()
--- userhandling/authenticateuser.py
+++ userhandling/authenticateuser.py
@@ -4,12 +4,11 @@ this is a stupid comment
 @author: Tor Larssen Sekse
 '''
 from passwordhandling.passwordsecurity import authenticate_password
-from userhandling.getuserdata import get_hashed_Password, get_salt
+from userhandling.getuserdata import get_hashed_Password
 from Databasehandling.queries import getuser
 
 def authenticate_user(email, password):
     user = getuser(email)
     hashedpassword = get_hashed_Password(user)
-    passwordsalt = get_salt(user)
     checker = authenticate_password(hashedpassword, password)
     return checker
