2d8f6046ce9995a513fcddc580de647ea2a1999d
Tor Larssen Sekse <tlskse@gmail.com>
2017-11-23 12:48:50 +0100

    renaming and comments

---
+++ main/data_handling.py
@@ -0,0 +1,7 @@
+'''
+Created on 12. okt. 2017
+The functions contained here connects with the 
+
+@author: Tor Larssen Sekse
+'''
+
--- main/remote_connection.py
+++ main/remote_connection.py
@@ -1,18 +1,53 @@
 '''
 Created on 19. okt. 2017
+remote_connection contains the functions that listen for incoming connections, connect and 
+wraps the socket used to connect in a TLS/SSL layer. It also contains the primary function
+for data processing. 
+
+Dataformats: 
+    all data sendt to the server over the secure socket should be in the following formats
+Login: 
+    To server: login|email|password
+    From server: login|boolean
+
+Getdata
+    To server: getdata|email|password|(getdatatype ex:getfirstname)|additionaldatatype|...
+    From server: getdata|(getdatatype ex:getfirstname)|data|additionalreturndata|...
+    Error: getdata|False|reason
+
+Getalldata
+    To server: getalldata|email|password
+    From server: getalldata|True|firstname|lastname|phone|postcode|country|countrycode|adress|adressnumber|birthday|gender
+    Error: 
+    getalldata|False|reason
+
+Newuser
+    To server: newuser|email|password|firstname|lastname|phone|postcode|country|countrycode|adress|adressnumber|birthday|gender
+    From server: newuser|boolean|reason
+
+Edituser
+    To server: edituser|email|password|(editdatatype ex:editfirstname)|newdata|additionaleditdatatype|additionalnewdata|...
+    From server: edituser|boolean|reason
+
+Deleteuser:
+    To server: deleteuser|email|password
+    From server: deleteuser|boolean|reason
 
 @author: Tor Larssen Sekse
 '''
+#Imports
+#there as a lot of imports because this is the primary location for executing functions
 import socket
 import ssl
 from _socket import SOL_SOCKET, SO_REUSEADDR
 from user_handling.authenticate_user import authenticate_user
-from user_handling.get_user_data import get_email, get_firstname, get_lastname,\
-    get_phonenumber, get_post_code, get_country, get_phone_Country, get_adress,\
-    get_adress_number, get_birthday, get_sex
 from database_handling.queries import get_user
 from user_handling.new_user import create_user
 from user_handling.delete_user import delete_user
+from user_handling.get_user_data import get_email, get_firstname, get_lastname,\
+    get_phonenumber, get_post_code, get_country, get_phone_Country, get_adress,\
+    get_adress_number, get_birthday, get_sex
+
 
 def handle_data(connstream, data):
     decoded_data =data.decode()
@@ -53,34 +88,34 @@ def handle_data(connstream, data):
                     return_data += b"|getfirstname|" + str(get_firstname(user)).encode()
                 
                 elif datalist[0] == "getlastname": 
-                    return_data = b"|getlastname|" + str(get_lastname(user)).encode()
+                    return_data += b"|getlastname|" + str(get_lastname(user)).encode()
                 
                 elif datalist[0] == "getphone": 
-                    return_data = b"|getphone|" + str(get_phonenumber(user)).encode()
+                    return_data += b"|getphone|" + str(get_phonenumber(user)).encode()
                 
                 elif datalist[0] == "getpostcode": 
-                    return_data = b"|getpostcode|" + str(get_post_code(user)).encode()
+                    return_data += b"|getpostcode|" + str(get_post_code(user)).encode()
                 
                 elif datalist[0] == "getcountry": 
-                    return_data = b"|getcountry|" + str(get_country(user)).encode()
+                    return_data += b"|getcountry|" + str(get_country(user)).encode()
                 
                 elif datalist[0] == "getcountrycode": 
-                    return_data = b"|getcountrycode|" + str(get_phone_Country(user)).encode()
+                    return_data += b"|getcountrycode|" + str(get_phone_Country(user)).encode()
                 
                 elif datalist[0] == "getadress": 
-                    return_data = b"|getadress|" + str(get_adress(user)).encode()
+                    return_data += b"|getadress|" + str(get_adress(user)).encode()
                 
                 elif datalist[0] == "getadressnumber": 
-                    return_data = b"getadressnumber|" + str(get_adress_number(user)).encode()
+                    return_data += b"getadressnumber|" + str(get_adress_number(user)).encode()
                 
                 elif datalist[0] == "getbirthday": 
-                    return_data = b"|getbirthday|" + str(get_birthday(user)).encode()
+                    return_data += b"|getbirthday|" + str(get_birthday(user)).encode()
                 
                 elif datalist[0] == "getgender": 
-                    return_data = b"|getgender|" + str(get_sex(user)).encode()
+                    return_data += b"|getgender|" + str(get_sex(user)).encode()
         
                 else:
-                    return_data = b"getdata|False|invalidquery"
+                    return_data += b"getdata|False|invalidquery"
                 del datalist[0]
             connstream.send(return_data)
         else:
