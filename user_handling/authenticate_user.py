a5bb4606f0c767e0614b0350333cd94b82844b6b
Tor Larssen Sekse <tlskse@gmail.com>
2017-11-23 12:22:59 +0100

    added comments to authenticateuser and done changes to change_user_data

--- user_handling/authenticate_user.py
+++ user_handling/authenticate_user.py
@@ -1,23 +1,32 @@
 '''
 Created on 5. okt. 2017
-Authenticate
+authenticate user contains all functions involved in authenticating the user on the server
+and the functions involved with handling passwords
 @author: Tor Larssen Sekse
 '''
-
+#imports functions to get needed data and bcrypt for the password handling
 from user_handling.get_user_data import get_hashed_Password
 from database_handling.queries import get_user
 import bcrypt
 
+#authenticate_user takes in an email adress and a password, then gets the userid and hashedpassword
+# from the database, and authenticates the user. 
 def authenticate_user(email, password):
+    #gathers data from the database
     user = get_user(email)
-    hashedpassword = get_hashed_Password(user)
-    checker = authenticate_password(hashedpassword, password)
+    hashed_password = get_hashed_Password(user)
+    #authenticates the password against the hashed one stored in the database
+    checker = authenticate_password(hashed_password, password)
+    #returns True/False depending on if the user was authenticated or not
     return checker
 
+#Generates a random salt for use in hashing the password
 def generate_salt():
     salt = bcrypt.gensalt()
     return salt
 
+#takes in hashed_password and password and runs bcrypt.checkpw function to determine if they
+#match. returns True/False
 def authenticate_password(hashed_password, password):
     checker = False
     if bcrypt.checkpw(password.encode(), hashed_password.encode()):
@@ -26,6 +35,7 @@ def authenticate_password(hashed_password, password):
         checker = False
     return checker
 
+#Takes in a salt and password and run bcrypt.hashpw and returns the hashed password
 def hash_password(salt, password):
-    hashedpassword = bcrypt.hashpw(password.encode(), salt)
-    return hashedpassword
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
--- user_handling/change_user_data.py
+++ user_handling/change_user_data.py
@@ -1,92 +1,117 @@
 '''
 Created on 12. okt. 2017
-
+The change_xxxx functions changes user data in the database. The user needs to be
+authenticated before any of these functions are called, because they don't authenticate
+the user before posting their data 
 @author: Tor Larssen Sekse
 '''
 from user_handling.input_control import *
-from user_handling.authenticate_user import authenticate_user
+import pymysql
 
-def change_email(user, password, email):
+
+def change_email(user, email):
     checker = False
     if check_email(email) == True:
-        authenticate_user(user, password)
-        #store new email in the database
+        db = pymysql.connect(host="88.88.170.2",  # your host 
+        user="server",       # username
+        passwd="sudoonlineid",     # password
+        db="OnlineID" # name of the database, commented out since I am creating the DB in the string below
+        )  
+ 
+        # Create a Cursor object to execute queries.
+        cur = db.cursor()
+        query = "UPDATE information SET email=%s WHERE user_id=%s"
+        cur.execute(query, (email, user))
+        # filter and return result  
+        db.commit()
+        checker = True
+        return checker
+
+    else:
+        return checker
+
+def change_firstname(user, first_name):
+    checker = False
+    if check_firstname(first_name) == True:
+        db = pymysql.connect(host="88.88.170.2",  # your host 
+        user="server",       # username
+        passwd="sudoonlineid",     # password
+        db="OnlineID" # name of the database, commented out since I am creating the DB in the string below
+            )  
+ 
+        # Create a Cursor object to execute queries.
+        cur = db.cursor()
+        query = "UPDATE information SET first_name=%s WHERE user_id=%s"
+        cur.execute(query, (first_name, user))
+        #filter and return result  
+        db.commit()
         checker = True
         return checker
     else:
         return checker
-
-def change_firstname(user_id, first_name):
-    import pymysql
-
-    db = pymysql.connect(host="88.88.170.2",  # your host 
+    
+def change_lastname(user, last_name):
+    checker = False
+    if check_lastname(last_name) == True:
+        db = pymysql.connect(host="88.88.170.2",  # your host 
         user="server",       # username
         passwd="sudoonlineid",     # password
         db="OnlineID" # name of the database, commented out since I am creating the DB in the string below
             )  
  
-# Create a Cursor object to execute queries.
-    cur = db.cursor()
-    query = "UPDATE information SET first_name=%s WHERE user_id=%s"
-    cur.execute(query, (first_name, user_id))
-    print("Changed first name of user_id", user_id, "to", first_name)
-# filter and return result  
-    db.commit()
+        # Create a Cursor object to execute queries.
+        cur = db.cursor()
+        query = "UPDATE information SET last_name=%s WHERE user_id=%s"
+        cur.execute(query, (last_name, user))
+        # filter and return result  
+        db.commit()
+        checker = True
+        return checker
+    else:
+        return checker
     
-def change_lastname(user_id, last_name):
-    import pymysql
-
-    db = pymysql.connect(host="88.88.170.2",  # your host 
+def change_phonenumber(user, phone_number):
+    checker = False
+    if check_phonenumber(phone_number) == True:
+        db = pymysql.connect(host="88.88.170.2",  # your host 
         user="server",       # username
         passwd="sudoonlineid",     # password
         db="OnlineID" # name of the database, commented out since I am creating the DB in the string below
             )  
  
-# Create a Cursor object to execute queries.
-    cur = db.cursor()
-    query = "UPDATE information SET last_name=%s WHERE user_id=%s"
-    cur.execute(query, (last_name, user_id))
-    print("Changed last name of user_id", user_id, "to", last_name)
-# filter and return result  
-    db.commit()
+        # Create a Cursor object to execute queries.
+        cur = db.cursor()
+        query = "UPDATE information SET phoneNumber=%s WHERE user_id=%s"
+        cur.execute(query, (phone_number, user))
+        # filter and return result  
+        db.commit()
+        checker = True
+        return checker
+    else:
+        return checker
     
-def change_phonenumber(user_id, phoneNumber):
-    import pymysql
-
-    db = pymysql.connect(host="88.88.170.2",  # your host 
+def change_adress(user, adress):
+    checker = False
+    if check_adress(adress) == True:
+        db = pymysql.connect(host="88.88.170.2",  # your host 
         user="server",       # username
         passwd="sudoonlineid",     # password
         db="OnlineID" # name of the database, commented out since I am creating the DB in the string below
             )  
  
-# Create a Cursor object to execute queries.
-    cur = db.cursor()
-    query = "UPDATE information SET phoneNumber=%s WHERE user_id=%s"
-    cur.execute(query, (phoneNumber, user_id))
-    print("Changed phone number of user_id", user_id, "to", phoneNumber)
-# filter and return result  
-    db.commit()
+    # Create a Cursor object to execute queries.
+        cur = db.cursor()
+        query = "UPDATE information SET adress=%s WHERE user_id=%s"
+        cur.execute(query, (adress, user))
+        # filter and return result  
+        db.commit()
+        checker = True
+        return checker
+    else:
+        return checker
     
-def change_adress(user_id, address):
-    import pymysql
-
-    db = pymysql.connect(host="88.88.170.2",  # your host 
-        user="server",       # username
-        passwd="sudoonlineid",     # password
-        db="OnlineID" # name of the database, commented out since I am creating the DB in the string below
-            )  
- 
-# Create a Cursor object to execute queries.
-    cur = db.cursor()
-    query = "UPDATE information SET adress=%s WHERE user_id=%s"
-    cur.execute(query, (address, user_id))
-    print("Changed address of user_id", user_id, "to", address)
-# filter and return result  
-    db.commit()
-    
-    
-def change_postcode(user_id, postcode):
-    import pymysql
+def change_postcode(user, postcode):
+    checker = False
 
     db = pymysql.connect(host="88.88.170.2",  # your host 
         user="server",       # username
@@ -103,7 +128,6 @@ def change_postcode(user_id, postcode):
     db.commit()
     
 def change_country(user_id, country):
-    import pymysql
 
     db = pymysql.connect(host="88.88.170.2",  # your host 
         user="server",       # username
@@ -120,7 +144,6 @@ def change_country(user_id, country):
     db.commit()
     
 def change_countrycode(user_id, countrycode):
-    import pymysql
 
     db = pymysql.connect(host="88.88.170.2",  # your host 
         user="server",       # username
@@ -138,7 +161,6 @@ def change_countrycode(user_id, countrycode):
     
     
 def change_birthday(user_id, birthday):
-    import pymysql
 
     db = pymysql.connect(host="88.88.170.2",  # your host 
         user="server",       # username
